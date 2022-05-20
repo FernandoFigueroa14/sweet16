8aba4cf13d4393ea2bca3e2580776a8c59cf4937
FernandoFigueroaR14 <fernando.figueroa@storicard.com>
2022-05-20 00:52:57 -0500

    cambios finales

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
     <Container className='hashtag'>
       <Row className="d-flex flex-column justify-content-center align-items-center text-center pt-4 text-light">
         <Col className="text-center pt-2">
-            <h4 className="fs-3 fw-lighter">NO TE OLVIDES DE USAR</h4>
+            <h4 className="fs-3 fw-light">DURANTE LA FIESTA NO TE OLVIDES DE USAR</h4>
             <h1 className="fs-1 fw-bold pb-3"> <strong>#Alefest</strong></h1>
         </Col>
         <Col className="text-center pb-4">
